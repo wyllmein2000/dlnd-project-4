350da7a28d818e8e038e1a424291a1d37625ce9a
wyllmein2000 <wyllmein2000@outlook.com>
2017-09-28 17:20:50 +0800

    fix a bug in seq2seq

--- seq2seq.py
+++ seq2seq.py
@@ -1,3 +1,6 @@
+from distutils.version import LooseVersion
+import problem_unittests as tests
+import warnings
 import tensorflow as tf
 import numpy as np
 import time
@@ -18,16 +21,41 @@ class seq2seq():
         self.rnn_size = rsize
         self.num_layers = nlayer
         self.keep_prob = kprob
-      
-        #self.train_logits = None
-        #self.inference_logits = None
-        #self.cost = None
-        #self.train_op = None
-        #self.train_graph
-        self.xxx = 1000
-        #self.build_model()
    
+
+    """ make sure you have the correct version of TensorFlow and access to a GPU """
+    def check_gpu(self):
+        # Check TensorFlow Version
+        assert LooseVersion(tf.__version__) in [LooseVersion('1.0.0'), LooseVersion('1.0.1')], 'This project requires TensorFlow version 1.0  You are using {}'.format(tf.__version__)
+        print('TensorFlow Version: {}'.format(tf.__version__))
+
+        # Check for a GPU
+        if not tf.test.gpu_device_name():
+            warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+        else:
+            print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+        
+        
+    def unit_test(self):
+        tests.test_sentence_to_seq(self.sentence_to_seq)
     
+        
+    def sentence_to_seq(self, sentence, vocab_to_int):
+        """
+        Convert a sentence to a sequence of ids
+        :param sentence: String
+        :param vocab_to_int: Dictionary to go from the words to an id
+        :return: List of word ids
+        """
+        output = []
+        for word in sentence.lower().split():
+            if word in vocab_to_int:
+                output.append(vocab_to_int[word])
+            else:
+                output.append(vocab_to_int['<UNK>'])
+        return output    
+        
+        
         
     """ Preprocess target data for decoding """
     def process_decoding_input(self, target_data):
@@ -91,88 +119,100 @@ class seq2seq():
         return train_logits, inference_logits
     
 
+    
+ 
 
-    #def train_op(self, train_source, train_target, valid_source, valid_target, save_path='checkpoints/dev'):
-    def build_model(self):
-        
+    def train_op(self, train_source, train_target, valid_source, valid_target, save_path='checkpoints/dev'):
+    
         #self.train_graph = tf.Graph()
         #with self.train_graph.as_default():
-
-            tf.reset_default_graph()
-             
-            # Create TF Placeholders for input, target, learning_rate and keep_prob
-            input_data = tf.placeholder(tf.int32, [None, None], name='input')
-            target_data = tf.placeholder(tf.int32, [None, None], name='target')
-            lrate = tf.placeholder(tf.float32, name='learning_rate')
-            kprob = tf.placeholder(tf.float32, name='keep_prob')
-            slength = tf.placeholder_with_default(self.max_source_sentence_length, None, name='sequence_length')
-            
-            input_shape = tf.shape(input_data)
-    
-            train_logits, inference_logits = self.seq2seq_model(tf.reverse(input_data, [-1]), target_data, slength, kprob)
+        tf.reset_default_graph()
         
-            #self.train_logits = train_logits
-            self.inference_logits = inference_logits
+        # Create TF Placeholders for input, target, learning_rate and keep_prob
+        input_data = tf.placeholder(tf.int32, [None, None], name='input')
+        target_data = tf.placeholder(tf.int32, [None, None], name='target')
+        lrate = tf.placeholder(tf.float32, name='learning_rate')
+        kprob = tf.placeholder(tf.float32, name='keep_prob')
+        slength = tf.placeholder_with_default(self.max_source_sentence_length, None, name='sequence_length')
+        
+        
+    
+        train_logits, inference_logits = self.seq2seq_model(tf.reverse(input_data, [-1]), target_data, slength, kprob)
+        
+        tf.identity(inference_logits, 'logits')
+        input_shape = tf.shape(input_data)
 
-            tf.identity(self.inference_logits, 'logits')
-            with tf.name_scope("optimization"):
-                # Loss function
-                self.cost = tf.contrib.seq2seq.sequence_loss(train_logits, target_data, tf.ones([input_shape[0], slength]))
+        
+        with tf.name_scope("optimization"):
+            # Loss function
+            cost = tf.contrib.seq2seq.sequence_loss(train_logits, target_data, tf.ones([input_shape[0], slength]))
 
-                # Optimizer
-                optimizer = tf.train.AdamOptimizer(lrate)
+            # Optimizer
+            optimizer = tf.train.AdamOptimizer(lrate)
 
-                # Gradient Clipping
-                gradients = optimizer.compute_gradients(self.cost)
-                capped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients if grad is not None]
-                self.train_op = optimizer.apply_gradients(capped_gradients)
+            # Gradient Clipping
+            gradients = optimizer.compute_gradients(cost)
+            capped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients if grad is not None]
+            train_op = optimizer.apply_gradients(capped_gradients)
                 
-            print('xxxxxx')
-            self.xxx = 10
-            
-        #print('yyyyyy')
-        #print(self.xxx)
-        #self.xxx = 100
- 
-    
- 
-
-    #def train_op(self, train_source, train_target, valid_source, valid_target, save_path='checkpoints/dev'):
-    def train_op(self):   
-    
-        print('train:', self.xxx)
+                
         
-        
-        
-        """
-        with tf.Session(graph=self.train_graph) as sess:
-        #with tf.Session() as sess:
+        with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             
-            print('hello')
-            print(self.xxx)
-        
+            start_time = time.time()
+            
             bsize = self.batch_size
             epoch = self.epochs
 
             for epoch_i in range(self.epochs):
                  for batch_i, (source_batch, target_batch) in enumerate(helper.batch_data(train_source, train_target, bsize)):
-                     #start_time = time.time()
+                     
             
-                     _, loss = sess.run([self.train_op, self.cost], {input_data: source_batch, target_data: target_batch, lrate: self.learning_rate, slength: target_batch.shape[1], kprob: self.keep_prob})
+                     _, loss = sess.run([train_op, cost], {input_data: source_batch, target_data: target_batch, lrate: self.learning_rate, slength: target_batch.shape[1], kprob: self.keep_prob})
             
-                     batch_train_logits = sess.run(self.inference_logits, {input_data: source_batch, kprob: 1.0})
-                     batch_valid_logits = sess.run(self.inference_logits, {input_data: valid_source, kprob: 1.0})
+                     batch_train_logits = sess.run(inference_logits, {input_data: source_batch, kprob: 1.0})
+                     batch_valid_logits = sess.run(inference_logits, {input_data: valid_source, kprob: 1.0})
                 
                      train_acc = self.get_accuracy(target_batch, batch_train_logits)
                      valid_acc = self.get_accuracy(np.array(valid_target), batch_valid_logits)
-                     #end_time = time.time()
-                     if batch_i % 100 == 0:
-                        print('Epoch {:>3} Batch {:>4}/{} - Train Accuracy: {:>6.3f}, Validation Accuracy: {:>6.3f}, Loss: {:>6.3f}'
-                           .format(epoch_i, batch_i, len(train_source) // bsize, train_acc, valid_acc, loss))
 
-        # Save Model
-        saver = tf.train.Saver()
-        saver.save(sess, save_path)
-        print('Model Trained and Saved')
-        """
+                     if batch_i % 100 == 0:
+                         print('Epoch {:>3} Batch {:>4}/{} - Train Accuracy: {:>6.3f}, Validation Accuracy: {:>6.3f}, Loss: {:>6.3f}'.format(epoch_i, batch_i, len(train_source) // bsize, train_acc, valid_acc, loss))
+
+            end_time = time.time()
+
+            # Save Model
+            saver = tf.train.Saver()
+            saver.save(sess, save_path)
+            print('Model Trained and Saved')
+            print('Cost time: {} sec'.format(end_time - start_time))
+        
+        
+        
+        
+    def translate(self, sentence, source_vocab_to_int, source_int_to_vocab, target_int_to_vocab, load_path):
+       
+        translate_sentence = self.sentence_to_seq(sentence, source_vocab_to_int)
+
+        loaded_graph = tf.Graph()
+        with tf.Session(graph=loaded_graph) as sess:
+            # Load saved model
+            loader = tf.train.import_meta_graph(load_path + '.meta')
+            loader.restore(sess, load_path)
+
+            input_data = loaded_graph.get_tensor_by_name('input:0')
+            logits = loaded_graph.get_tensor_by_name('logits:0')
+            keep_prob = loaded_graph.get_tensor_by_name('keep_prob:0')
+
+            translate_logits = sess.run(logits, {input_data: [translate_sentence], keep_prob: 1.0})[0]
+
+        print('Input')
+        print('  Word Ids:      {}'.format([i for i in translate_sentence]))
+        print('  English Words: {}'.format([source_int_to_vocab[i] for i in translate_sentence]))
+
+        print('\nPrediction')
+        print('  Word Ids:      {}'.format([i for i in np.argmax(translate_logits, 1)]))
+        print('  French Words: {}'.format([target_int_to_vocab[i] for i in np.argmax(translate_logits, 1)]))
+        output = [target_int_to_vocab[i] for i in np.argmax(translate_logits, 1)]
+        return output
